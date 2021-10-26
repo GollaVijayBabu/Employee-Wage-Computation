0442eb92ee55bfc201a6d23bb30cce29b7b7daa6
GollaVijayBabu <gollavijaybabu221@gmail.com>
2021-10-26 05:49:19 -0700

    modified program

---
+++ Employee Wage Compitation/Attendance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee_Wage_Computation
+{
+    public class Attendance
+    {
+        Random random = new Random();
+        public string name;
+        public void presnt()
+        {
+            int EmpCheck = random.Next(0, 2);
+            if (EmpCheck == 1)
+                Console.WriteLine(name + " Employee is present");
+            else
+                Console.WriteLine(name + " Employee is absent");
+        }
+    }
+}
---
+++ Employee Wage Compitation/Program.cs
@@ -0,0 +1,16 @@
+﻿ 
+using System;
+
+namespace Employee_Wage_Computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Employe Wage Computation ");
+            Attendance uc_1 = new Attendance();
+            uc_1.name = "vijay";
+            uc_1.presnt();
+        }
+    }
+}
