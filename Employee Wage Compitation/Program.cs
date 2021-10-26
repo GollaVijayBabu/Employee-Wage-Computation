8f0281ca0deee0d29d06dc28ace7ade7e2048fe1
pc <pc@DESKTOP-I3IDQFQ>
2021-10-26 04:41:56 -0700

    Add project files.

---
+++ Employee Wage Compitation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Employe Wage Computation ");
+        }
+    }
+}
